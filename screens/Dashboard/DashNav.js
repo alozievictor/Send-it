5c5686437a670d3cef5c8cacdee6246b1c7ea0e7
alozievictor <aloziev50@gmail.com>
2023-10-17 03:15:22 +0100

    okay changed

--- screens/Dashboard/DashNav.js
+++ screens/Dashboard/DashNav.js
@@ -3,11 +3,6 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Dashboard from "./dashboard";
-import Wallet from "./Wallet";
-// import NewOrder from "../RequestOrder/NewOrder";
-// import ReDets from "./ReDets";
-// import Payment from "../RequestOrder/Payment";
-// import ProfileNav from "../Profile/ProfileNav";
 
 const Stack = createNativeStackNavigator();
 
@@ -20,11 +15,6 @@ const DashNav = () => {
         }}
       >
         <Stack.Screen name="dashboard" component={Dashboard} />
-        <Stack.Screen name="Wallet" component={Wallet} />
-        {/* <Stack.Screen name="Neworder" component={NewOrder}/>
-        <Stack.Screen name="Recieve" component={ReDets}/>
-        <Stack.Screen name="Payment" component={Payment}/> */}
-        
       </Stack.Navigator>
   );
 };
