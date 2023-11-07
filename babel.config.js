a1e9a1bec280a9adeb080984a5bf1db8602c8cb6
alozievictor <aloziev50@gmail.com>
2023-11-07 14:34:16 +0100

    another changes

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      require.resolve("expo-router/babel"),
-      "react-native-reanimated/plugin",
-    ],
-    env: {
-      production: {
-        plugins: ["react-native-paper/babel"],
-      },
-    },
+    plugins:[
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+    ]
   };
 };
+
+
+
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: [
+//       require.resolve("expo-router/babel"),
+//       "react-native-reanimated/plugin",
+//     ],
+//     env: {
+//       production: {
+//         plugins: ["react-native-paper/babel"],
+//       },
+//     },
+//   };
+// };
